202c8ae5e5fe7f295c535dfe9584096872250580
WeiChungChang <r97922153@gmail.com>
2018-08-18 07:30:21 +0800

    fix newaxis bug

--- Practice/python_newaxis/newaxis.py
+++ Practice/python_newaxis/newaxis.py
@@ -1,15 +1,17 @@
 import numpy as np
 x1 = np.array([10, 20, 30], float)
-print "shape of x1 is ", x1.shape
-print x1
+print("shape of x1 is ", x1.shape)
+print(x1)
+print('')
 
 x2 = x1[:, np.newaxis]
-print "shape of x2 is ", x2.shape
-print x2
+print("shape of x2 is ", x2.shape)
+print(x2)
+print('')
 
 x3 = x1[np.newaxis, :]
-print "shape of x3 is ", x2.shape
-print x2
+print("shape of x3 is ", x3.shape)
+print(x3)
 
 '''
 ---result---
@@ -19,8 +21,6 @@ shape of x2 is  (3, 1)
 [[ 10.]
  [ 20.]
  [ 30.]]
-shape of x3 is  (3, 1)
-[[ 10.]
- [ 20.]
- [ 30.]]
+shape of x3 is  (1, 3)
+[[ 10.  20.  30.]]
 '''
